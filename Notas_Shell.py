02bd1dca1efdb0f5eeb7bf2d45c59c9dad172909
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-01-04 20:22:08 -0600

    Rama experimental, restructurar todote

---
+++ Notas_Shell.py
@@ -0,0 +1,223 @@
+import os
+
+from logic.Modulo_System import (
+    get_system,
+    CleanScreen
+)
+from logic.Modulo_Text import (
+    Text_Read
+)
+from interface.Modulo_ShowPrint import (
+    Title,
+    Continue,
+    Separator
+)
+from data.Modulo_Language import (
+    get_text as Lang,
+    YesNo
+)
+from data.Modulo_Notas import (data_Nota, read_Nota, save_Nota, get_list as Nota_get_list)
+
+
+#path = 'Notes/'
+#path_main = os.getcwd()
+
+
+def Menu_Main():
+    loop = True
+    while loop == True:
+        # Parte visual
+        CleanScreen()
+        Title('Notas')
+        text = (
+            f'1. {Lang("new_note")}.\n'
+            f'2. {Lang("edit_note")}.\n'
+            f'3. {Lang("remove_note")}\n'
+            f'4. {Lang("change_main_dir")}\n'
+            f'0. {Lang("exit")}'
+        )
+        option = input(
+            text + '\n'
+            f"{Lang('set_option')}: "
+        )
+        
+        # Opcion elegida
+        if option == '1':
+            New_Note()
+
+        elif option == '2':
+            Edit_Note()
+
+        elif option == '3':
+            Remove_Note()
+        
+        elif option == '4':
+            Change_Path_Note()
+
+        elif option == '0':
+            loop = False
+
+        else:
+            pass
+
+
+def New_Note():
+    # Parte visual
+    CleanScreen()
+    Title(Lang('new_note'))
+    text = input(f'{Lang("name")}: ')
+    
+    # Texto de input
+    if text == '':
+        # Si no escribio nada entonces, el texto sera texto
+        text = 'texto'
+    else:
+        # Normal, el imput es correcto.
+        pass
+    
+    # Crear el archivo necesario.
+    save_Nota( data_Nota, save=text )
+    text_ready = data_Nota.note
+    if type(text_ready) is str:
+        # Abrir nano en base al texto creado
+        if get_system() == 'linux':
+            os.system(f'nano "{text_ready}"')
+        elif get_system() == 'win':
+            os.system(f'notepad "{text_ready}"')
+
+    elif type(text_ready) is list:
+        # El texto creado ya existe, y abrirlo con nano.
+        input(
+            Lang('this_file_exists') + '\n'
+            f'{Lang("continue_enter")}...'
+        )
+        if get_system() == 'linux':
+            os.system(f'nano "{text_ready[1]}"')
+        elif get_system() == 'win':
+            os.system(f'notepad "{text_ready[1]}"')
+
+    else:
+        # Fallo en la creación del archivo
+        # O El input tiene caracteres erroneos
+        # Hay un Error
+        input(
+            f'ERROR - {Lang("error_create_file")}\n'
+            f'{Lang("continue_enter")}...'
+        )
+
+
+def Edit_Note():
+    # Parte visual
+    CleanScreen()
+    Title(Lang('edit_note'))
+    text = f'1. {Lang("last_note")}\n'
+
+    number = 1
+    dict_text = {}
+    for note in Nota_get_list( data_Nota ):
+        number += 1
+        dict_text.update({ str(number) : note })
+    
+    for key in dict_text.keys():
+        text += f'{key}. {dict_text[key]}\n'
+
+    option = input (
+        text + '\n'
+        f'{Lang("set_option")}: '
+    )
+    
+    # Opcion elegida
+    if option == '1':
+        # Ultimo texto creado
+        edit = data_Nota.note
+        if type(edit) is str:
+            if get_system() == 'linux':
+                os.system(f'nano "{edit}"')
+            elif get_system() == 'win':
+                os.system(f'notepad "{edit}"')
+        elif edit == None:
+            input(
+                f'{Lang("no_note")}.\n'
+                f'{Lang("continue_enter")}...'
+            )
+
+    elif option in dict_text:
+        # Elegir una nota, entre las notas existentes.
+        data_Nota.last_note = dict_text[option]
+        save_Nota( data_Nota )
+        edit = data_Nota.note
+        if get_system() == 'linux':
+            os.system(f'nano "{edit}"')
+        elif get_system() == 'win':
+            os.system(f'notepad "{edit}"')
+
+    else:
+        # No hacer nada, no existe esa opcion
+        pass
+
+
+def Remove_Note():
+    # Parte visual
+    CleanScreen()
+    Title(Lang('remove_note'))
+
+    number = 0
+    dict_text = {}
+    for note in Nota_get_list( data_Nota ):
+        number += 1
+        dict_text.update({ str(number) : note })
+
+    text = ''
+    for key in dict_text.keys():
+        text += f'{key}. {dict_text[key]}\n'
+
+    option = input (
+        text + '\n'
+        f'{Lang("set_option")}: '
+    )
+    
+    # Opcion elegida
+    if option in dict_text:
+        # Eleguir una nota, entre las notas existentes.
+        remove = save_Nota( data_Nota, remove=dict_text[option] )
+        if remove == True:
+            # Se pudo remover
+            print(Lang('remove_good'))
+        else:
+            # No se pudo remover
+            print(Lang('remove_not_good'))
+            
+        input(f'{Lang("continue_enter")}...')
+
+    else:
+        # No hacer nada, no existe esa opcion
+        pass
+
+
+def Change_Path_Note():
+    CleanScreen()
+    Title(Lang("dir_main"))
+    print(
+        f'{Lang("dir_current")}:\n' +
+        data_Nota.path + '\n'
+    )
+    option = Continue(
+        Lang("change_main_dir")
+    )
+    
+    if option == YesNo('yes'):
+        data_Nota.path = input(f'{Lang("dir")}: ')
+        new_path = save_Nota( data_Nota )
+        if new_path == True:
+            print( Lang('dir_change_good') )
+        elif new_path == False:
+            print( Lang('dir_change_not_good') )
+
+        input(f'{Lang("continue_enter")}...')
+
+    elif option == YesNo('no'):
+        pass
+
+
+if __name__ == '__main__':
+    Menu_Main()
---
+++ entities/Nota.py
@@ -0,0 +1,8 @@
+last_note = str
+# Ultima nota creada o accedida.
+
+path = str
+# Directorio de las notas.
+
+note = str
+# Note
